b7d32ca32f1ccb1c138496905f24aac5c4e5daed
Nicola Pellegrini <90765299+Niguoz@users.noreply.github.com>
2022-11-16 10:37:53 +0100

    Player Controller

---
+++ Assets/Code/Character/PlayerController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private Player _playerController;
+    private Rigidbody _rb;
+    private Vector2 _movementInput;
+    private Vector3 _moveOnZ;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null)
+        {
+            Debug.LogError("NO RIGIDBODY");
+        }
+
+        _playerController = new Player();
+    }
+
+    private void OnEnable()
+    {
+        _playerController.PlayerMovement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerController.PlayerMovement.Disable();
+    }
+
+    private void Update()
+    {
+        _movementInput = _playerController.PlayerMovement.Move.ReadValue<Vector2>();
+        _moveOnZ = new Vector3(_movementInput.x, 0, _movementInput.y);
+        _rb.velocity = _moveOnZ * _speed;
+    }
+}
